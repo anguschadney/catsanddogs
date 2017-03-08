9d618f9f7a3f80ed091da7e3969da4a8901b3a9a
anguschadney <angus.chadney@gmail.com>
2017-03-08 16:29:55 +0000

    refactored and added docstring

--- rainnext.py
+++ rainnext.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python
+"""
+Simple script to hook into the DarkSky API and tell the user when it will
+rain next.
+
+Expects a settings.py file in the same directory with the contents:
+api_key = ''
+latitude = ''
+longitude = ''
+
+Powered by Dark Sky https://darksky.net/poweredby
+"""
+
 from collections import OrderedDict
 from datetime import datetime
 import requests
@@ -7,12 +19,9 @@ from settings import api_key, latitude, longitude
 
 
 base_url = 'https://api.darksky.net/forecast/{}/{},{}'
-normal_output_str = 'There will be rain at {}, the forecast is showing {}.'
-rain_now_output_str = 'It\'s raining now!  The forecast is showing {}.'
+normal_output_str = 'There will be rain at {}, the forecast is showing {}'
+rain_now_output_str = 'It\'s raining now!  The forecast is showing {}'
 precip_threshold = 0.3
-
-url = base_url.format(api_key, latitude, longitude)
-forecast = requests.get(url).json()
 
 time_fmt = OrderedDict()
 time_fmt['minutely'] = '%I:%M %p'
@@ -24,11 +33,28 @@ def convert_time(epoch_time):
     return datetime.fromtimestamp(epoch_time)
 
 
+def get_forecast():
+    url = base_url.format(api_key, latitude, longitude)
+    forecast = requests.get(url).json()
+
+    for period in time_fmt.keys():
+        rain_next = next(
+            (
+                fc for fc in forecast[period]['data']
+                if fc['precipProbability'] > precip_threshold
+            ),
+            None,
+        )
+        if rain_next:
+            break
+    return period, rain_next
+
+
 def get_rain_forecast(forecast):
     try:
-        return rain_next['summary'].lower()
+        return forecast['summary'].lower()
     except KeyError:
-        return rain_next['precipType'].lower()
+        return forecast['precipType'].lower()
 
 
 def format_output(rain_next, period):
@@ -36,22 +62,13 @@ def format_output(rain_next, period):
     rain_forecast = get_rain_forecast(rain_next)
     delta = (rain_time - datetime.now()).total_seconds()
 
-    if delta <= 60:
+    if delta <= 60 and period is not 'daily':
         return rain_now_output_str.format(rain_forecast)
 
     rain_time_str = rain_time.strftime(time_fmt[period])
     return normal_output_str.format(rain_time_str, rain_forecast)
 
 
-for period in time_fmt.keys():
-    rain_next = next(
-        (
-            fc for fc in forecast[period]['data']
-            if fc['precipProbability'] > precip_threshold
-        ),
-        None,
-    )
-    if rain_next:
-        break
-
-print(format_output(rain_next, period))
+if __name__ == '__main__':
+    period, rain_next = get_forecast()
+    print(format_output(rain_next, period))
